f2232a9d3760ad46a00e46a91f93c64ccb55a067
Nattanant Kampiranont <144806771+tonhomsanforwork@users.noreply.github.com>
2023-11-23 10:55:21 +0700

    CS264_Workshop_6509650393

---
+++ TDD6509650393/src/core/Stack.java
@@ -0,0 +1,72 @@
+package core;
+
+public class Stack implements IStack 
+{
+	
+	private int size;		//ความจุ
+	private int top = -1;	//index element ตัวบนสุด
+	private Object[] stack; //Array of Object
+	
+	Stack(int size)
+	{
+		this.size = size;
+		stack = new Object[size];
+		
+	}
+	@Override
+	public int getSize() 
+	{
+		return this.top + 1;
+	}
+
+	@Override
+	public boolean isEmpty() 
+	{
+		return top == -1;
+	}
+
+	@Override
+	public boolean isFull() 
+	{
+		return top == size-1;
+	}
+
+	@Override
+	public void push(Object ele) throws Exception 
+	{
+		if(isEmpty()) 
+		{
+			top++;
+			stack[top] = ele;
+			return;
+		}
+		if(isFull()) 
+		{
+			throw new Exception("Stack is Full");		
+		}
+		if(!(ele.getClass().getName() == top().getClass().getName())) 
+		{
+			throw new Exception("All elements in the stack must be of the same type");
+		}
+		
+		top++;
+		stack[top] = ele;
+		
+	}
+
+	@Override
+	public Object pop() 
+	{
+		top--;
+		return stack[top+1];
+	}
+	@Override
+	public Object top() 
+	{
+		if(isEmpty()) 
+		{
+			return -1;
+		}
+		return stack[top];
+	}
+}
